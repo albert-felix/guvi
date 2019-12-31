3104dfc26f4ecc0ea0fb3abc736b2df30b7e5964
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2019-12-31 19:43:17 +0530

    Add files via upload

--- loops/lcm.py
+++ loops/lcm.py
@@ -1,10 +1,11 @@
-a = int(input())    
-b = int(input())
+value_1 = int(input('Enter first number : '))    
+value_2 = int(input('Enter second number : '))
 i = 1
 
-while a>0 and b>0:
-    lcm = a*i
-    if lcm%b == 0:
+while True:
+    lcm = value_1*i
+    if lcm%value_2 == 0:
+        print('LCM : ',end='')
         print(lcm)
         break
     i += 1
