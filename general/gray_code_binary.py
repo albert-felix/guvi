7b494f2e1eab29b29e1ef3b0db076d1b00e6a04a
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-06-29 22:49:27 +0530

    Update gray_code_binary.py

--- general/gray_code_binary.py
+++ general/gray_code_binary.py
@@ -1,3 +1,5 @@
+// Binary base gray code
+
 n = int(input())
 bit = ''
 
@@ -16,5 +18,24 @@ for j in range((2**n) -1):
             temp.pop(k)
             temp.insert(k,'0')
     result.append("".join(temp))
+
+print(*result, sep=" ")
+
+// No base gray code
+
+n = int(input())
+
+result = ['0', '1']
+
+if n > 1:
+    for i in range(n-1):
+        for j in result[::-1]:
+            result.append(j)
+    
+    for k in range(0,int(len(result)/2)):
+        result[k] = "0" + result[k]
+    
+    for l in range(int(len(result)/2), len(result)):
+        result[l] = "1" + result[l]
 
 print(*result, sep=" ")
