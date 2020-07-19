9617bd39e4f246b866668582e71f735fa344bc1d
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-07-19 10:09:10 +0530

    Create pattern_5.py

---
+++ general/pattern_5.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        if j == 1:
+            print('*', end="")
+        else:
+            print('*', end="  ")
+    print()
